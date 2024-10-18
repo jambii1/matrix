3f8a20d36f2ba7087518f1b67a35216ac410f859
jambii1 <s.maslevtsov@gmail.com>
2024-10-18 20:44:20 +0300

    Makefile and main.cpp modification matrix.hpp and matrix.cpp created matrix functions declared in matrix.hpp matrix functions defined in matrix.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
+#include "matrix.hpp"
 #include <iostream>
-#include <cstddef>
 #include <exception>
-
-int **create(size_t m, size_t n);
-void read(int **table, size_t m, size_t n);
-void write(const int * const *table, size_t m, size_t n);
-void clean(int **table, size_t m);
 
 int main()
 {
@@ -27,33 +22,13 @@ int main()
     return 1;
   }
 
+  read(table, m, n);
+  if (!std::cin)
+  {
+    std::cerr << "Incorrect input\n";
+    return 1;
+  }
+  write(table, m, n);
+
   clean(table, m);
-}
-
-int **create(size_t m, size_t n)
-{
-  int **table = new int *[m];
-  size_t created = 0;
-  try
-  {
-    for (; created < m; ++created)
-    {
-      table[created] = new int[n];
-    }
-  }
-  catch (const std::bad_alloc &e)
-  {
-    clean(table, created);
-    throw;
-  }
-  return table;
-}
-
-void clean(int **table, size_t m)
-{
-  for (size_t i = 0; i < m; ++i)
-  {
-    delete[] table[i];
-  }
-  delete[] table;
 }
